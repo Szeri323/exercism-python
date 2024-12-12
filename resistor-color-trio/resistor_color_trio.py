22852aea71352d0f7ffc49bdc821bdbaf45675fa
Szeri323 <szeri323@gmail.com>
2024-12-12 18:02:13 +0100

    update resistor color trio

--- resistor-color-trio/resistor_color_trio.py
+++ resistor-color-trio/resistor_color_trio.py
@@ -1,4 +1,4 @@
-def label(colors):
+def color_to_number(color):
     colors_dict = {
         "black": 0,
         "brown": 1,
@@ -11,36 +11,32 @@ def label(colors):
         "grey": 8,
         "white": 9
     }
+    return colors_dict[color]
+
+def label(colors):
+    first = color_to_number(colors[0])
+    second = color_to_number(colors[1])
+    third = color_to_number(colors[2])
+
     value = 0
     zeros = 0
-    if colors_dict[colors[1]] != 0 :
-        value = colors_dict[colors[0]] * 10 + colors_dict[colors[1]]
-        zeros = colors_dict[colors[2]]
+    if second != 0:
+        value = first * 10 + second
+        zeros = third
     else:
-        value = colors_dict[colors[0]]
-        zeros = colors_dict[colors[2]] + 1 
+        value = first
+        zeros = third + 1 
     if value == 0:
         zeros = 0
     number = str(value)
     zeros_str = zeros * "0"
     if zeros > 2:
+        number += zeros % 3 * "0"
         if zeros >= 3 and zeros <= 5:
-            if zeros == 4:
-                number += "0"
-            if zeros == 5:
-                number += "00"
             number += " kilo"
         if zeros >= 6 and zeros <= 8:
-            if zeros == 7:
-                number += "0"
-            if zeros == 8:
-                number += "00"
             number += " mega"
         if zeros >= 9 and zeros <= 11:
-            if zeros == 10:
-                number += "0"
-            if zeros == 11:
-                number += "00"
             number += " giga"
     else:
         number += zeros_str + " ohms"
@@ -49,7 +45,3 @@ def label(colors):
     number += "ohms"
     return number
 
-print(label(["orange", "orange", "orange"]))
-print(label(["red", "black", "red"]))
-print(label(["blue", "green", "yellow"]))
-print(label(["yellow", "violet", "yellow"]))

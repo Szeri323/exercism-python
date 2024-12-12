ba4db7c234d84509313102abbaf8dcb607821ca9
Szeri323 <szeri323@gmail.com>
2024-12-12 18:30:32 +0100

    update resistor color trio

--- resistor-color-trio/resistor_color_trio.py
+++ resistor-color-trio/resistor_color_trio.py
@@ -18,30 +18,17 @@ def label(colors):
     second = color_to_number(colors[1])
     third = color_to_number(colors[2])
 
-    value = 0
-    zeros = 0
-    if second != 0:
-        value = first * 10 + second
-        zeros = third
+    value = (10 * first + second) * 10 ** third
+    if value > 1_000_000_000:
+        prefix = "giga"
+        value //= 1_000_000_000
+    elif value > 1_000_000:
+        prefix = "mega"
+        value //= 1_000_000 
+    elif value > 1_000:
+        prefix = "kilo"
+        value //= 1_000
     else:
-        value = first
-        zeros = third + 1 
-    if value == 0:
-        zeros = 0
-    number = str(value)
-    zeros_str = zeros * "0"
-    if zeros > 2:
-        number += zeros % 3 * "0"
-        if zeros >= 3 and zeros <= 5:
-            number += " kilo"
-        if zeros >= 6 and zeros <= 8:
-            number += " mega"
-        if zeros >= 9 and zeros <= 11:
-            number += " giga"
-    else:
-        number += zeros_str + " ohms"
-        return number
-
-    number += "ohms"
-    return number
+        prefix = ""
+    return f"{value} {prefix}ohms"
 
